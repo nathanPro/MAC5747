176480d1503f8bbb9ad444dbfdf3343d051dade4
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-11-06 16:09:28 -0200

    Python first version

--- Delaunay/Delaunay.py
+++ Delaunay/Delaunay.py
@@ -2,14 +2,8 @@ from collections import namedtuple
 Point = namedtuple('Point', 'x y')
 Epoint = namedtuple('EPoint', 'inf inner')
 
-def make_point(*, x, y):
-    return Point(x, y)
-
-def cross(a, b):
+def area2(a, b):
     return a.x * b.y - a.y * b.x
-
-def make_e_point(*, x, y, inf = False):
-    return Epoint(inf = inf, inner = make_point(x = x, y = y))
 
 def promote(p, inf = False):
     if not isinstance(p, Epoint):
@@ -23,12 +17,69 @@ def left(a, b):
     def inner(c):
         c = promote(c)
         p = [0, 0, 0]
-        points = [a, b, c, a]
+        points = [a, b, c]
         for i in range(3):
-            rank = points[i].inf + points[i + 1].inf
-            p[rank] += cross(points[i].inner, points[i + 1].inner)
+            rank = points[i - 1].inf + points[i].inf
+            p[rank] += area2(points[i - 1].inner, points[i].inner)
         i = 2
         while (i > 0 and p[i] == 0): i -= 1;
         return p[i] > 0;
 
     return inner
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.P = [a, b, c]
+        self.A = [None, None, None]
+        self.C = None
+        assert left(a, b)(c)
+
+    def contains(self, q):
+        for i in range(3):
+            if left(self.P[i], self.P[i - 1])(q):
+                return False
+        return True
+
+    def __str__(self):
+        return ' '.join(str(p) for p in self.P)
+
+class DAG:
+    def leaf(T):
+        return T.C is None
+
+    def split_triangle(T, p):
+        assert DAG.leaf(T)
+
+        ans = []
+        aux = [p, T.P[1], T.P[2]]
+        for i in range(3):
+            ans.append(Triangle(aux[0], aux[1], aux[2]))
+            if i + 1 < 3:
+                aux[i] = T.P[i]
+                aux[i + 1] = p
+
+        aux = [T.A[0], ans[1], ans[2]]
+        for i in range(3):
+            ans[i].A = aux.copy()
+            if T.A[i] is not None:
+                assert DAG.leaf(T.A[i])
+                T.A[i].A[T.A[i].A.index(T)] = ans[i]
+            if i + 1 < 3:
+                aux[i] = ans[i]
+                aux[i + 1] = T.A[i + 1]
+
+        T.C = ans
+
+    def __init__(self):
+        self.root = Triangle(promote(Point(-1, -1), True),
+                             promote(Point(1, -1), True),
+                             promote(Point(0, 1), True))
+
+    def find(self, p):
+        ans = self.root
+        while not DAG.leaf(ans):
+            ans = next(T for T in ans.C if T.contains(p))
+        return ans
+
+    def insert(self, p):
+        DAG.split_triangle(self.find(p), p)
--- Delaunay/test.py
+++ Delaunay/test.py
@@ -1,7 +1,42 @@
 from Delaunay import *
+
 comp = left(Point(0, 2), Point(2, 2))
-assert(comp(Point(1, 1)) == False)
-assert(comp(promote(Point(1, 1), True)) == True)
+assert comp(Point(1, 1)) == False
+assert comp(promote(Point(1, 1), True))
 
 comp = left(Point(2, 0), Point(2, 2))
-assert(comp(promote(Point(0, 1), True)))
+assert comp(promote(Point(0, 1), True))
+
+T = Triangle(a = promote(Point(1, -1), True),
+             b = promote(Point(0, 1), True),
+             c = promote(Point(-1, -1), True))
+
+assert T.contains(Point(10213123123, 1823719823))
+
+division = DAG();
+
+n = int(input())
+for i in range(n):
+    x, y = [int(s) for s in input().split()]
+    print("Inserting", Point(x, y))
+    division.insert(Point(x, y))
+
+n = int(input())
+for i in range(n):
+    x, y = (int(s) for s in input().split())
+    p = Point(x, y)
+    print(p, ':', division.find(p))
+
+def dfs(T, seen = None):
+    print('dfs', T)
+    for V in T.A:
+        print(" " * 4, V)
+    if seen is None:
+        seen = set()
+
+    seen.add(T)
+    for V in T.A:
+        if V is not None and V not in seen:
+            dfs(V, seen)
+
+dfs(division.find(Point(1, 1)))
