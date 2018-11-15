9ba19abb8865614d17f7dd665a149fa673819c75
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-11-15 12:41:03 -0200

    Interface uses my algo

--- Delaunay/geocomp-py-framework/geocomp/__init__.py
+++ Delaunay/geocomp-py-framework/geocomp/__init__.py
@@ -12,6 +12,7 @@ Sub-modulos:
 
 from . import convexhull
 from . import farthest
+from . import delaunay
 from .common.guicontrol import init_display
 from .common.guicontrol import config_canvas
 from .common.guicontrol import run_algorithm
@@ -20,7 +21,8 @@ from .common.prim import get_count
 from .common.prim import reset_count
 
 children = (   ( 'convexhull', None, 'Fecho Convexo' ),
-		( 'farthest',  None, 'Par Mais Distante' )
+		( 'farthest',  None, 'Par Mais Distante' ),
+		( 'delaunay',  None, 'Triangulação de Delaunay' )
 	)
 
 __all__ = [p[0] for p in children]
---
+++ Delaunay/geocomp-py-framework/geocomp/delaunay/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""Algoritmos para construir a triangulação de Delaunay
+
+Algoritmos disponiveis:
+- Incremental Probabilistico
+
+algoritmo otimo = executa em tempo O(n lg(n)), n = numero de pontos
+"""
+from . import incr_prob
+
+# cada entrada deve ter:
+#  [ 'nome-do-modulo', 'nome-da-funcao', 'nome do algoritmo' ]
+children = [
+	( 'incr_prob', 'IncrProb', 'Incremental\nProbabilistico' )
+]
+
+#children = algorithms
+
+#__all__ = [ 'graham', 'gift' ]
+__all__ = [a[0] for a in children]
